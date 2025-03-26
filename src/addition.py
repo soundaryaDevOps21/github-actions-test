8811eab9af468dfb73d37e49bf0088c5fff5d56d
soundaryaDevOps21 <ksoundarya9545@gmail.com>
2025-03-26 19:06:15 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -6,6 +6,6 @@ def test_addition(a, b):
 # Example usage
 num1 = 10
 num2 = 20
-result = add_numbers(num1, num2)
+result = test_addition(num1, num2)
 
 print(f"The sum of {num1} and {num2} is: {result}")
